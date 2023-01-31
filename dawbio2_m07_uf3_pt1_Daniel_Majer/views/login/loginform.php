19f69b9b6225d01e432ab8f951677bff59a88a34
heloint <majerdaniel93@gmail.com>
2023-01-31 13:45:33 +0100

    retry login when logged in restriction DONE

--- dawbio2_m07_uf3_pt1_Daniel_Majer/views/login/loginform.php
+++ dawbio2_m07_uf3_pt1_Daniel_Majer/views/login/loginform.php
@@ -15,7 +15,6 @@ $disabledClass = "";
 // and login is correctly validated,
 // then disable the login form.
 if (isset($_SESSION["username"])) {
-    echo '<p style="text-align: center; color:red;">You are already logged in!</p>';
     $disabled = "disabled";
 }
 
@@ -33,54 +32,64 @@ if (isset($params["invalidUsername"]) &&
 }
 // ================================================
 
-// Print out the login form.
-echo <<<EOT
-<div class="container">
-    <form class="row d-flex justify-content-center" action="index.php" method="post" >
+if (!isset($_SESSION['username']) &&
+    !isset($_SESSION['userrole'])) {
 
-        <div class="col-3">
-            <label for="username">Username</label>
+    // Print out the login form.
+    echo <<<EOT
+    <div class="container">
+        <form class="row d-flex justify-content-center" action="index.php" method="post" >
+
+            <div class="col-3">
+                <label for="username">Username</label>
+                <div class="w-100"></div>
+                <input id="username"
+                       name="username"
+                       type="text"
+                       placeholder="Username .."
+                       value="{$invalidUsername}"
+                      {$disabled}
+                >
+            </div>
             <div class="w-100"></div>
-            <input id="username"
-                   name="username"
-                   type="text"
-                   placeholder="Username .."
-                   value="{$invalidUsername}"
-                  {$disabled}
-            >
-        </div>
-        <div class="w-100"></div>
 
-        <div class="col-3 my-3">
-            <label for="password">Password</label>
+            <div class="col-3 my-3">
+                <label for="password">Password</label>
+                <div class="w-100"></div>
+                <input id="password"
+                       name="password"
+                       type="password"
+                       placeholder="Password .."
+                       value="{$invalidPassword}"
+                      {$disabled}
+                >
+            </div>
             <div class="w-100"></div>
-            <input id="password"
-                   name="password"
-                   type="password"
-                   placeholder="Password .."
-                   value="{$invalidPassword}"
-                  {$disabled}
-            >
+
+            <div class="col-3" >
+                <button class="btn btn-secondary"
+                        id="login"
+                        name="action"
+                        value="user/login"
+                        type="submit" {$disabled} >
+                            Login
+                </button>
+            </div>
+
+        </form>
+
+    </div>
+
+    <br>
+    <div class="input-wrapper" style="display:block;text-align:center;">
+        <p style="text-align:center;color:red;">{$message}</p>
+    </div
+
+    EOT;
+} else {
+    echo <<<EOT
+        <div class="d-flex justify-content-center align-items-center">
+            <h4 class="text-danger">Already logged in!</h4>
         </div>
-        <div class="w-100"></div>
-
-        <div class="col-3" >
-            <button class="btn btn-secondary"
-                    id="login"
-                    name="action"
-                    value="user/login"
-                    type="submit" {$disabled} >
-                        Login
-            </button>
-        </div>
-
-    </form>
-
-</div>
-
-<br>
-<div class="input-wrapper" style="display:block;text-align:center;">
-    <p style="text-align:center;color:red;">{$message}</p>
-</div
-
-EOT;
+    EOT;
+}
