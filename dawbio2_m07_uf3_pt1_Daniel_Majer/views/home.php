6c9b4b3a8f0aea36017a85f147f94f6e05c4bb5a
heloint <majerdaniel93@gmail.com>
2023-01-31 13:19:34 +0100

    home page done

--- dawbio2_m07_uf3_pt1_Daniel_Majer/views/home.php
+++ dawbio2_m07_uf3_pt1_Daniel_Majer/views/home.php
@@ -1,3 +1,26 @@
 <?php
-echo "<p>Welcome to Store manager</p>";
-//TODO add some content, such as text and images that fit a main page for a store web page.
+
+if (isset($_SESSION['username']) &&
+    isset($_SESSION['role'])) {
+echo <<<EOT
+    <div class="d-flex justify-content-start align-items-center">
+        <img src="./images/anonym-user-profile.png" style="height:8rem;width:8rem;">
+        <div>
+            <p>{$_SESSION['username']}</p> 
+            <p>{$_SESSION['role']}</p> 
+        </div>
+    </div>
+EOT;
+}
+
+echo <<<EOT
+    '<h3 class="text-center">Welcome to Store manager</h3>';
+
+    <div class="d-flex justify-content-center align-items-center">
+        <img src="./images/Store-Manager.jpg">
+    </div>
+
+    <div class="d-flex justify-content-center align-items-center">
+        <p>Where you can manage as much as you want...</p>
+    </div>
+EOT;
